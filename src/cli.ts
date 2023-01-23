61cf550647435f8e4d7a350596ef68851b86fec1
Mantton <mantton.official@gmail.com>
2023-01-22 20:03:55 -0500

    1.3.0

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@
 import { Command } from "commander";
 import { build } from "./commands/build";
 import { serve } from "./commands/serve";
+import { getVersion } from "./utils/getDaisukeVersion";
 const json = require("../package.json");
 
 const program = new Command();
@@ -11,14 +12,25 @@ program.name("daisuke").description("Suwatte Dev Tools").version(json.version);
 
 program
   .command("build")
+  .alias("b")
   .description("Builds valid runners in project directory")
   .option("--noList", "Skips generating the runner list.")
   .action(build);
 
 program
   .command("serve")
+  .alias("s")
   .description("Builds & Serves a List on the local network")
   .option("-p, --port <string>", "Port server should run on.")
   .action(serve);
 
-program.parse();
+// TODO: Generate Command
+// program
+//   .command("generate <schematic> [name]")
+//   .alias("g")
+//   .action(generate)
+//   .description("Generate a Standard Runner or Template Class");
+
+program
+  .version(getVersion(), "-v, --version", "Display the version number")
+  .parse(process.argv);
---
+++ src/utils/getDaisukeVersion.ts
@@ -0,0 +1,10 @@
+import type { PackageJson } from "type-fest";
+import path from "path";
+import fs from "fs-extra";
+export const getVersion = () => {
+  const packageJsonPath = path.join("../../", "package.json");
+
+  const packageJsonContent = fs.readJSONSync(packageJsonPath) as PackageJson;
+
+  return packageJsonContent.version ?? "1.0.0";
+};
